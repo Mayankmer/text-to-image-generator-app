5c7b7717149ded8fba4c1295e42ad04f8597099a
Mayankmer <mayankmer24@gmail.com>
2025-08-29 01:49:21 +0530

    Refactor App component by removing unused useState import; enhance Description component with new layout and content; add newline at end of index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/NavBar'
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 function Description() {
   return (
@@ -10,6 +11,20 @@ function Description() {
         <p className='text-gray-500 mb-8'>
             Turn your imagination into visuals
         </p>
+        <div className='flex flex-col gap-5 md:gap-14 md:flex-row
+        items-center'>
+            <img src={assets.sample_img_1} alt="" className='w-80
+            xl:w-96 rounded-lg'/>
+            <div className='text-left'>
+            <h2 className='text-3xl font-medium max-w-lg mb-4'>Introducing the AI-Powered text to image generator</h2>
+            <p className='text-gray-600 mb-4'>Turn your ideas into art, instantly. 
+                Whether you're a writer looking to illustrate your story,
+                 a designer seeking inspiration, or just a curious mind, 
+                 our text-to-image app provides a seamless and magical experience. 
+                 Just describe your vision, and in moments, 
+                you'll have a unique piece of visual art that was born from your words alone.</p>
+                </div>
+        </div>
     </div>
   )
 }
